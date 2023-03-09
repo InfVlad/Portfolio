d8b1c8a7e05569a722b6c97770439a46e4543409
SpicyArepa <infantevladimir@gmail.com>
2023-03-09 18:11:42 -0400

    added send email feature

--- app/message/route.ts
+++ app/message/route.ts
@@ -1,11 +1,29 @@
+import nodemailer from "nodemailer";
+
 // eslint-disable-next-line import/prefer-default-export
 export async function POST(request: Request) {
-    // do something
-    // const { name, email, message } = request.body.;
+    try {
+        const { name, email, message } = await request.json();
 
-    const { name, email, message } = await request.json();
-    console.log("name ", name);
-    console.log("email ", email);
-    console.log("message ", message);
-    return new Response(`${name} thank you for your message!`);
+        const transporter = nodemailer.createTransport({
+            host: process.env.EMAIL_SERVICE,
+            port: 587,
+            secure: false,
+            auth: {
+                user: process.env.EMAIL_USER,
+                pass: process.env.EMAIL_PASS,
+            },
+        });
+
+        const info = await transporter.sendMail({
+            from: process.env.EMAIL_SENDER,
+            to: process.env.EMAIL_DESTINY,
+            subject: "Hello! someone sent you a message from your Portfolio!",
+            text: `${name} \nemail: ${email}\nSays: ${message}`,
+        });
+        console.log("Message sent: %s", info.messageId);
+        return new Response(`${name} thank you for your message!`);
+    } catch (error) {
+        console.error(error);
+    }
 }
