5011ed0f419aec8acbc8c3afc4f7c0fac7bc8240
SpicyArepa <infantevladimir@gmail.com>
2023-03-10 21:29:14 -0400

    added footer, formated global styles with prettier

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,5 +1,27 @@
 const Footer = () => {
-    return <div className="text-white">Footer</div>;
+    return (
+        <div className="footer w-full text-white">
+            <div className="mx-auto flex flex-col items-center justify-center py-8">
+                <div className="pb-2">
+                    <p>
+                        This site was built with NextJs, you can check the code{" "}
+                        <span className="text-primary-blue">
+                            <a
+                                href="https://github.com/InfVlad/Portfolio"
+                                target="_blank"
+                                aria-label="checkout the github page for more information"
+                                rel="noopener noreferrer"
+                            >
+                                {" "}
+                                here!
+                            </a>
+                        </span>
+                    </p>
+                </div>
+                <p>Vladimir Infante. 2023</p>
+            </div>
+        </div>
+    );
 };
 
 export default Footer;
