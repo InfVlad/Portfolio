b08d0984bc67d5b674bc5c6e089767ce6ef33921
SpicyArepa <infantevladimir@gmail.com>
2023-03-17 11:18:19 -0400

    added OG metadata for social media links

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,19 @@ import { Radio_Canada } from "next/font/google";
 export const metadata = {
     title: "Vladimir Infante - Portfolio",
     description: "Vladimir Infante's Personal Portfolio",
+    keywords: ["Frontend", "Fullstack", "Developer", "Next.js"],
+    openGraph: {
+        title: "Vladimir Infante - Portfolio",
+        description: "Vladimir Infante's Developer Portfolio built with NextJs 13.2",
+        images: [
+            {
+                url: "public/assets/portfolio-home-preview.png",
+                width: 752,
+                height: 400,
+                alt: "portfolio thumbnail",
+            },
+        ],
+    },
 };
 const radioCanada = Radio_Canada({
     subsets: ["latin"],
